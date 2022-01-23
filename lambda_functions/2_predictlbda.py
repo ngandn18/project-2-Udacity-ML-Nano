767feca20fa16d0b216bdec3bdc432d6513f17c8
Thien Ngan Doan <46005184+ngandn18@users.noreply.github.com>
2022-01-23 16:04:58 -0600

    Update 2_predictlbda.py
    Add comment of the useful link to run this function.

--- lambda_functions/2_predictlbda.py
+++ lambda_functions/2_predictlbda.py
@@ -9,14 +9,15 @@ import boto3
 ENDPOINT_NAME = 'image-classification-2021-11-21-03-39-30-277'
 runtime= boto3.client('runtime.sagemaker')
 
-def lambda_handler(event, context):
+# The notebook' guide shows the impossible import sagemaker. 
+# Mentor shows a simple way to call runtime.invoke_runtime to run this lambda function. Pls read the following link.
+# https://aws.amazon.com/blogs/machine-learning/call-an-amazon-sagemaker-model-endpoint-using-amazon-api-gateway-and-aws-lambda/
 
+def lambda_handler(event, context):
     # Decode the image data
     payload = base64.b64decode(event["image_data"])## TODO: fill in
-
     # Instantiate a Predictor
-    # predictor = ## TODO: 
-    # endpoint_name='image-classification-2021-11-21-03-39-30-277'
+    endpoint_name='image-classification-2021-11-21-03-39-30-277'
     # For this model the IdentitySerializer needs to be "image/png"
     try:
         response = runtime.invoke_endpoint(
@@ -32,9 +33,6 @@ def lambda_handler(event, context):
 
     except Exception as e:
         print(f'Inference Error: {e}')
-
-    # Make a prediction:
-    # inferences = ## TODO: fill in
 
     # We return the data back to the Step Function    
     return json.dumps(event)    
